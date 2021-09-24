713f9eb06dc16f49a8159176e36431dc4ff347e1
Gil <gil@indeed.com>
2021-09-24 14:06:35 -0500

    update webpack config

--- webpack.js
+++ webpack.js
@@ -22,28 +22,7 @@ module.exports = (env, argv) => {
       extensions: ["*", ".js"]
     },
     optimization: {
-      minimize: argv.mode === 'production',
-      splitChunks: {
-        chunks: 'all',
-        minSize: 20000,
-        minRemainingSize: 0,
-        minChunks: 1,
-        maxAsyncRequests: 30,
-        maxInitialRequests: 30,
-        enforceSizeThreshold: 50000,
-        cacheGroups: {
-          defaultVendors: {
-            test: /[\\/]node_modules[\\/]/,
-            priority: -10,
-            reuseExistingChunk: true,
-          },
-          default: {
-            minChunks: 2,
-            priority: -20,
-            reuseExistingChunk: true,
-          },
-        },
-      }
+      minimize: argv.mode === 'production'
     },
     plugins: [
       new SentryCliPlugin({
